1e65cc40c20682b7eb892d6871cb1d0898cc1793
Mugi473 <ismaigoundiam@gmail.com>
2026-05-05 17:31:13 +0000

    Strip to Crunchyscan only and reconfigure workflow for single-repo publish

---
+++ src/fr/crunchyscan/src/eu/kanade/tachiyomi/extension/fr/crunchyscan/Crunchyscan.kt
@@ -0,0 +1,16 @@
+package eu.kanade.tachiyomi.extension.fr.crunchyscan
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class Crunchyscan : Madara(
+    "Crunchyscan",
+    "https://crunchyscan.fr",
+    "fr",
+    SimpleDateFormat("MMMM d, yyyy", Locale("fr")),
+) {
+    override val useNewChapterEndpoint = true
+    override val mangaDetailsSelectorStatus =
+        "div.summary-heading:contains(statut) + div.summary-content"
+}
