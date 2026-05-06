68cff5e31d68e15dcd03ad97b331cc01e1b3d5f7
Mugi473 <ismaigoundiam@gmail.com>
2026-05-06 12:32:48 +0000

    Fix ktlint format for Crunchyscan.kt

--- src/fr/crunchyscan/src/eu/kanade/tachiyomi/extension/fr/crunchyscan/Crunchyscan.kt
+++ src/fr/crunchyscan/src/eu/kanade/tachiyomi/extension/fr/crunchyscan/Crunchyscan.kt
@@ -4,12 +4,13 @@ import eu.kanade.tachiyomi.multisrc.madara.Madara
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class Crunchyscan : Madara(
-    "Crunchyscan",
-    "https://crunchyscan.fr",
-    "fr",
-    SimpleDateFormat("MMMM d, yyyy", Locale("fr")),
-) {
+class Crunchyscan :
+    Madara(
+        "Crunchyscan",
+        "https://crunchyscan.fr",
+        "fr",
+        SimpleDateFormat("MMMM d, yyyy", Locale("fr")),
+    ) {
     override val useNewChapterEndpoint = true
     override val mangaDetailsSelectorStatus =
         "div.summary-heading:contains(statut) + div.summary-content"
